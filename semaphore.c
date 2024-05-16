d4b1284b85d8dd115b4badcf351da10339a4c1f3
Yasmine <yasmine.atef2004@gmail.com>
2024-05-17 02:38:07 +0300

    workingSemaphore

--- semaphore.c
+++ semaphore.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdio.h>
 #include "semaphore.h"
 
 void semWait(semaphore *s, int processID)
@@ -6,9 +7,16 @@ void semWait(semaphore *s, int processID)
     if (s->ava)
     {
         s->ava = false;
+        s->ownerID = processID;
+        printf("Resource is now acquired by process %i\n", processID);
+    }
+    else if (s->ownerID == processID)
+    {
+        printf("Resource is already acquired by the process");
     }
     else
     {
+        printf("Process %i is now blocked for the resource\n", processID);
         enqueue(&(s->blockedQueue), processID);
     }
 }
@@ -19,12 +27,14 @@ void semSignal(semaphore *s, int processID)
     {
         if (!isEmpty(&(s->blockedQueue)))
         {
-            int processID = dequeue(&(s->blockedQueue));
-            s->ownerID = processID;
+            int dequeuedProcessID = dequeue(&(s->blockedQueue));
+            printf("Resource is now acquired by process %i\n", dequeuedProcessID);
+            s->ownerID = dequeuedProcessID;
         }
         else
         {
             s->ava = true;
+            printf("Resource is now available\n");
         }
     }
 }
